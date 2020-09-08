7767f4d630e5fee311e87bb0d95ecfe13c505c35
ronngo <ronngomn23@gmail.com>
2020-09-08 21:21:40 +0700

    first commit

---
+++ hienthiloichao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hienthiloichao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:  ");
+            String name = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Hello : " + name);
+            Console.ReadKey();
+        }
+    }
+}
