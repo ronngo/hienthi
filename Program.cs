efc7d1714e8d50455929016c2a4fbb5590cea6af
ronngo <70925598+ronngo@users.noreply.github.com>
2020-09-09 20:47:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hienthiloichao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hthi();
+        }
+        static void hthi()
+        {
+            Console.WriteLine("Enter your name ?");
+            String name = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Hello :{0}  ", name);
+
+        }
+    }
+}
